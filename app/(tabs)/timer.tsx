b412829fbac4864092d731e05ccc7683653a77ce
Zahwan69 <zahuwansul@gmail.com>
2025-11-24 19:16:35 +0500

    initial setup with Expo Router, navigation and dependencies.

--- app/(tabs)/timer.tsx
+++ app/(tabs)/timer.tsx
@@ -9,6 +9,7 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <p>HAH</p>
     </View>
   );
 }
